daa695e724a726a3c62cea5ebcb947d7ae5622dc
thykers <58331450+thykers@users.noreply.github.com>
2020-01-06 10:31:51 +0100

    Add files via upload

--- features.py
+++ features.py
@@ -3,17 +3,88 @@ This file is for all the feature-methods
 """
 import numpy as np
 import dictionaries as dt
+from sklearn.pipeline import FeatureUnion
 from sklearn.feature_extraction.text import CountVectorizer
 
-def get_features(data, path, filenames_list):
-    X = number_of_words(data, path, filenames_list)
+vector = None
+vectorizer = CountVectorizer()
+
+def get_features(data, path_to_articles, filenames_list, is_training):
+    union = FeatureUnion([("A", length_of_span(data)), ("B", number_of_words(data, path_to_articles, filenames_list))])
+    """
+    if is_training:
+        X = initialize_vectoriser(data)
+    else:
+        X = transform_data_to_countvector(data)
+
+    X = np.hstack((X, relative_capitals_frequency(data, path_to_articles, filenames_list)))
+    X = np.hstack((X, relative_punctuation_frequency(data, path_to_articles, filenames_list)))
+    X = np.hstack((X, average_number_of_words_per_sentence(data, path_to_articles, filenames_list)))
+    X = np.hstack((X, number_of_words(data, path_to_articles, filenames_list)))
     X = np.hstack((X, length_of_span(data)))
+
     return X
+    """
+    return union
+
+def relative_punctuation_frequency(data, path, filenames):
+    punctuation_list = ['.', ',', '?', '!', ';', ':', '\"', '\'', '[', ']', '(', ')', '/', '{', '}']
+    dict_rel_punc_frequency = dict()
+    X = []
+    for line, filename in zip(data, filenames):
+        if(filename in dict_rel_punc_frequency.keys()):
+            X.append(dict_rel_punc_frequency[filename])
+        else:
+            f = open(path / filename, encoding="utf-8")
+            text = f.read()
+            punctuation_count = 0
+            for n, c in enumerate(text):
+                if(c in punctuation_list):
+                    punctuation_count += 1
+            dict_rel_punc_frequency[filename] = punctuation_count / n
+            X.append(punctuation_count / n)
+    return np.array(X).reshape(-1, 1)
+
+def averaged_word_length(data, path, filenames):
+    dict_of_avg_word_len = dict()
+    X = []
+    for line, filename in zip(data, filenames):
+        if(filename in dict_of_avg_sent_len.keys()):
+            X.append(dict_of_avg_sent_len[filename])
+        else:
+            character_count = 0
+            f = open(path / filename, encoding="utf-8")
+            text = f.read()
+            words = text.split(" ")
+            for n, word in enumerate(words):
+                character_count += len(word.replace("\n", ""))
+            dict_of_avg_word_len[filename] = character_count / n
+            X.append(character_count / n)
+    return np.array(X).reshape(-1, 1)
 
 def length_of_span(data):
     return np.array([len(span) for span in data]).reshape(-1, 1)
 
+def average_number_of_words_per_sentence(data, path, filenames):
+    dict_of_avg_sent_len = dict()
+    X = []
+    for line, filename in zip(data, filenames):
+        if(filename in dict_of_avg_sent_len.keys()):
+            X.append(dict_of_avg_sent_len[filename])
+        else:
+            total_words = 0
+            f = open(path / filename, encoding="utf-8")
+            lines = f.readlines()
+            for n, line in enumerate(lines):
+                total_words += len(line)
+            dict_of_avg_sent_len[filename] = total_words / n
+            X.append(total_words / n)
+    return np.array(X).reshape(-1, 1)
+
 def number_of_words(data, path, filenames):
+    """
+    Number of Words in the Article
+    """
     article_dict_of_word_count = dict()
     X = []
     for line, filename in zip(data, filenames):
@@ -27,12 +98,30 @@ def number_of_words(data, path, filenames):
             X.append(len(words))
     return np.array(X).reshape(-1, 1)
 
-def initialize_vectoriser(data):
-    vectorizer = CountVectorizer()
-    vectors = vectorizer.fit_transform(data)
+def relative_capitals_frequency(data, path, filenames):
+    dict_rel_cap_frequency = dict()
+    X = []
+    for line, filename in zip(data, filenames):
+        if(filename in dict_rel_cap_frequency.keys()):
+            X.append(dict_rel_cap_frequency[filename])
+        else:
+            f = open(path / filename, encoding="utf-8")
+            text = f.read()
+            capital_count = 0
+            for n, c in enumerate(text):
+                if(c.isupper()):
+                    capital_count += 1
+            dict_rel_cap_frequency[filename] = capital_count / n
+            X.append(capital_count / n)
+    return np.array(X).reshape(-1, 1)
 
-def transform_data_to_countvector(data, vector):
-    return vector.transform(data)
+def initialize_vectoriser(data):
+    global vector
+    vector = vectorizer.fit_transform(data)
+    return np.array(vector).reshape(-1, 1)
+
+def transform_data_to_countvector(data):
+    return vectorizer.transform(data)
 
 def emotion_annontation(data):
     X = []
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def read_from_file(path):
         f = open(path / filename, encoding="utf-8")
         for span in f.readlines():
             spans.append(span.replace("\n", ""))
-            filename_of_article.append(filename)
+            filename_of_article.append(filename[18:])
     f.close()
     return spans, filename_of_article
 
@@ -22,14 +22,14 @@ training_data_folder = Path("./Data/datasets/alternative-train-spans-task2/")
 training_label_folder = Path("./Data/datasets/alternative-train-spans-labels-task2/")
 prediction_folder = Path("./Data/datasets/alternative-dev-spans-task2/")
 gold_label_folder = Path("./Data/datasets/alternative-dev-spans-labels-task2/")
+article_folder = Path("./Data/datasets/train-articles/")
 
 logreg_classifier = LogisticRegression(penalty='l2', class_weight='balanced', solver="lbfgs", multi_class='auto')
-vectorizer = CountVectorizer()
 
 if __name__ == "__main__":
 
     training_spans, filename_of_article_per_span = read_from_file(training_data_folder)
-    X = ft.get_features(training_spans, training_data_folder, filename_of_article_per_span)
+    X = ft.get_features(training_spans, article_folder, filename_of_article_per_span, True)
 
     training_labels, filename_of_label = read_from_file(training_label_folder)
     Y = np.array([dt.category_to_number_dict[y] for y in training_labels])
@@ -37,18 +37,13 @@ if __name__ == "__main__":
     logreg_classifier.fit(X, Y)
 
     spans, filenames_of_article = read_from_file(prediction_folder)
-    X = ft.get_features(spans, prediction_folder, filenames_of_article)
+    X = ft.get_features(spans, article_folder, filenames_of_article, False)
 
     y_pred = logreg_classifier.predict(X)
     gold_labels, filename_of_label = read_from_file(gold_label_folder)
     y_true = np.array([dt.category_to_number_dict[x] for x in gold_labels])
 
     ev.print_score(y_pred, y_true)
-
-#vectors = vectorizer.fit_transform(training_spans)
-#logreg_classifier.fit(vectors, Y)
-#vector = vectorizer.transform(spans)
-#y_pred = logreg_classifier.predict(vector)
 
 #X = np.hstack((X, vector.toarray()))
 #X = np.hstack((X, vectors.toarray()))
